db57a0c971503d19ae385437bccbe7e336b06aa6
YourGitHubUsername <your-email@example.com>
2025-07-09 15:59:15 -0400

    Add viewport meta for mobile layouts

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,9 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        {/* Crucial for proper mobile layouts */}
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
         {/* Preconnect for performance */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link 
@@ -13,7 +16,8 @@ export default function App({ Component, pageProps }) {
           href="https://fonts.gstatic.com" 
           crossOrigin="true" 
         />
-        {/* Load Inter (400 & 700); swap={{display:'swap'}} */}
+
+        {/* Load Inter (400 & 700); swap ensures text remains visible during load */}
         <link 
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" 
           rel="stylesheet" 
